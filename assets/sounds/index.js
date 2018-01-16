624fc0236a43f3e2fa1ac60f97d27efd27793868
Douglas Leite <douglas.leite@vocado.com>
2018-01-16 19:59:44 -0400

    talk2go: adding sound assets

--- assets/sounds/index.js
+++ assets/sounds/index.js
@@ -2,56 +2,56 @@ export default {
     Brazil: {
         Portuguese: {
             greetings: {
-                g1: require('./greetings/br-g1.mp3'),
-                g2: require('./greetings/br-g2.mp3'),
-                g3: require('./greetings/br-g3.mp3'),
-                g4: require('./greetings/br-g4.mp3'),
-                g5: require('./greetings/br-g5.mp3'),
-                g6: require('./greetings/br-g6.mp3'),
+                g1: require('./greetings/pt-BR-g1.mp3'),
+                g2: require('./greetings/pt-BR-g2.mp3'),
+                g3: require('./greetings/pt-BR-g3.mp3'),
+                g4: require('./greetings/pt-BR-g4.mp3'),
+                g5: require('./greetings/pt-BR-g5.mp3'),
+                g6: require('./greetings/pt-BR-g6.mp3'),
             }
         }
     },
     Switzerland: {
         German: {
             greetings: {
-                g1: require('./greetings/ge-g1.mp3'),
-                g2: require('./greetings/ge-g2.mp3'),
-                g3: require('./greetings/ge-g3.mp3'),
-                g4: require('./greetings/ge-g4.mp3'),
-                g5: require('./greetings/ge-g5.mp3'),
-                g6: require('./greetings/ge-g6.mp3'),
+                g1: require('./greetings/de-DE-g1.mp3'),
+                g2: require('./greetings/de-DE-g2.mp3'),
+                g3: require('./greetings/de-DE-g3.mp3'),
+                g4: require('./greetings/de-DE-g4.mp3'),
+                g5: require('./greetings/de-DE-g5.mp3'),
+                g6: require('./greetings/de-DE-g6.mp3'),
             }
         },
         French: {
             greetings: {
-                g1: require('./greetings/fr-g1.mp3'),
-                g2: require('./greetings/fr-g2.mp3'),
-                g3: require('./greetings/fr-g3.mp3'),
-                g4: require('./greetings/fr-g4.mp3'),
-                g5: require('./greetings/fr-g5.mp3'),
-                g6: require('./greetings/fr-g6.mp3'),
+                g1: require('./greetings/fr-FR-g1.mp3'),
+                g2: require('./greetings/fr-FR-g2.mp3'),
+                g3: require('./greetings/fr-FR-g3.mp3'),
+                g4: require('./greetings/fr-FR-g4.mp3'),
+                g5: require('./greetings/fr-FR-g5.mp3'),
+                g6: require('./greetings/fr-FR-g6.mp3'),
             }
         },
         Italian: {
             greetings: {
-                g1: require('./greetings/it-g1.mp3'),
-                g2: require('./greetings/it-g2.mp3'),
-                g3: require('./greetings/it-g3.mp3'),
-                g4: require('./greetings/it-g4.mp3'),
-                g5: require('./greetings/it-g5.mp3'),
-                g6: require('./greetings/it-g6.mp3'),
+                g1: require('./greetings/it-IT-g1.mp3'),
+                g2: require('./greetings/it-IT-g2.mp3'),
+                g3: require('./greetings/it-IT-g3.mp3'),
+                g4: require('./greetings/it-IT-g4.mp3'),
+                g5: require('./greetings/it-IT-g5.mp3'),
+                g6: require('./greetings/it-IT-g6.mp3'),
             }
         }
     },
     'United States of America': {
         English: {
             greetings: {
-                g1: require('./greetings/us-g1.mp3'),
-                g2: require('./greetings/us-g2.mp3'),
-                g3: require('./greetings/us-g3.mp3'),
-                g4: require('./greetings/us-g4.mp3'),
-                g5: require('./greetings/us-g5.mp3'),
-                g6: require('./greetings/us-g6.mp3'),                
+                g1: require('./greetings/en-US-g1.mp3'),
+                g2: require('./greetings/en-US-g2.mp3'),
+                g3: require('./greetings/en-US-g3.mp3'),
+                g4: require('./greetings/en-US-g4.mp3'),
+                g5: require('./greetings/en-US-g5.mp3'),
+                g6: require('./greetings/en-US-g6.mp3'),                
             }
         }
     }
